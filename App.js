e634831b6ab0432931156a7553b4c30a1fe8fecd
Joel <joel_mpy@hotmail.com>
2022-04-07 13:39:38 +0200

    react-exercice-2

--- App.js
+++ App.js
@@ -1,21 +1,43 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect } from "react";
+import { View, Text, FlatList, Image } from "react-native";
+import { useState } from "react";
 
-export default function App() {
+function App() {
+  const [countries, setCountries] = useState([]);
+
+  useEffect(() => {
+    const url = "https://restcountries.com/v3.1/all";
+
+    fetch(url)
+      .then(function (res) {
+        return res.json();
+      })
+      .then(function (response) {
+        let listItem = response.map((country) => {
+          return { name: country.name.common, capital: country.capital, image : country.flags.png };
+        });
+        setCountries(listItem);
+      });
+  }, []);
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View>
+      <Text>Salut a tous ici Konexio</Text>
+      <Text>{JSON.stringify(countries.length)}</Text>
+      <FlatList
+        data={countries}
+        renderItem={(item) => {
+          return (
+            <View>
+              <Text>{item.item.name}</Text>
+              <Text>{item.item.capital}</Text>
+              <Image style= { {width : 100, height : 100 }} source={item.item.image}/>
+            </View>
+          );
+        }}
+      ></FlatList>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
